b3ec33f451ea6253b219060b15782964637f7a8b
James Lloyd <jijlloyd98@gmail.com>
2022-03-23 18:05:53 +0000

    day one, basic stuff added

--- flutter_personal_website/lib/main.dart
+++ flutter_personal_website/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'James Lloyd Personal Website',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'James Lloyd Personal Website'),
     );
   }
 }
@@ -48,68 +49,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                MaterialButton(
+                  child: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(10),
+                    child: Text("My Books"),
+                  ),
+                  onPressed: () async {
+                    const url =
+                        "https://jameslloyd.notion.site/Read-Books-84f527d45fe7496b889f9a742cee26a6";
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                MaterialButton(
+                  child: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(10),
+                    child: Text("My Photos"),
+                  ),
+                  onPressed: () async {
+                    const url = "https://www.instagram.com/fujijlloyd/";
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
+                SizedBox(height: 50),
+                MaterialButton(
+                  child: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(10),
+                    child: Text("Travels in Guatemala"),
+                  ),
+                  onPressed: () async {
+                    const url =
+                        "https://jameslloyd.notion.site/Guatemala-69c45eeb29af45b4a929d876d346291d";
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
+                SizedBox(height: 50),
+                MaterialButton(
+                  child: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(10),
+                    child: Text("Playlists"),
+                  ),
+                  onPressed: () async {
+                    const url =
+                        "https://open.spotify.com/user/8jgj19mnukoyzb805mfibdhvk?si=B5ymIuSnRkuofMO-H_MGHA";
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+              height: 70,
+              color: Colors.blue,
+              child: Center(
+                  child: Text(
+                'Made by James Lloyd using Flutter',
+                style: TextStyle(color: Colors.white),
+              )),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
